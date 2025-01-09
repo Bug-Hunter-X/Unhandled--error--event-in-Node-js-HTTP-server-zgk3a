7ac68cc885ef96b35a2b16d1e6191789de97d855
BugHunter-X <askzenai@gmail.com>
2025-01-09 04:22:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+```
